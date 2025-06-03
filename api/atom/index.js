e4c26c8ecb535bf16d9123299d96db85ee0e4b63
Rebeca Ramirez <rebecarv27@gmail.com>
2025-06-03 00:23:37 -0700

    eeeeeeeee

--- api/atom/index.js
+++ api/atom/index.js
@@ -1,28 +1,4 @@
-import express from "express";
-import serverless from "serverless-http";
-import morgan from "morgan";
-import cors from "cors";
-import routeAPI from "./atom/routes/index.js";
-import config from "../src/config/config.js";
-import { mongoose } from "../src/config/database.config.js";
-
-const app = express();
-
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-const api = config.API_URL || "/api";
-
-app.get(`${api}`, (req, res) => {
-  res.send(`<h1>RESTful running in root</h1> <p> atomic: <b>${api}/api-docs</b></p>`);
-});
-
-app.get('/atomic', (req, res) => {
-  res.send(`<h1>RESTful running in YVAN</h1> <p> eCommerce: <b>${api}/api-docs</b></p>`);
-});
-
-routeAPI(app);
+import serverless from 'serverless-http';
+import app from '../src/app.js'; // ✅ Importa tu app Express
 
 export const handler = serverless(app);
--- src/app.js
+++ src/app.js
@@ -1,33 +1,28 @@
-import morgan from "morgan";
-import express from "express";
-import cors from "cors";
-import routeAPI from "../api/atom/routes/index";
-import config from "./config/config";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import routeAPI from './api/atom/routes/index.js';
+import config from './config/config.js';
+import { mongoose } from './config/database.config.js';
 
 const app = express();
 
-import { mongoose } from "./config/database.config";
-
-app.set('port',config.PORT);
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-const api = config.API_URL;
-app.get(`${api}`, (req,res)=>{
-    res.send(
-        `<h1>RESTful running in root</h1> <p> atomic: <b>${api}/api-docs</b> for more information.</p>`
-    );
-})
-app.get('/atomic', (req,res)=>{
-    res.send(
-        `<h1>RESTful running in YVAN</h1> <p> eCommerce: <b>${api}/api-docs</b> for more information.</p>`
-    );
-})
-// Routes routeAPI(app);
+const api = config.API_URL || '/api';
+
+app.get(`${api}`, (req, res) => {
+  res.send(`<h1>RESTful running in root</h1> <p>atomic: <b>${api}/api-docs</b></p>`);
+});
+
+app.get('/atomic', (req, res) => {
+  res.send(`<h1>RESTful running in YVAN</h1> <p>eCommerce: <b>${api}/api-docs</b></p>`);
+});
+
 routeAPI(app);
-// Swagger Docs
-// Middleware para el manejo de errores
-// Export App
+
 export default app;
