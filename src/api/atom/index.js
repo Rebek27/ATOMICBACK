6ef999aea14bdc98c5fe3fe80fdbdb72efe5c001
Rebeca Ramirez <rebecarv27@gmail.com>
2025-06-02 23:47:31 -0700

    ss

--- src/api/atom/index.js
+++ src/api/atom/index.js
@@ -14,9 +14,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const api = config.API_URL || "/api";
+
 app.get(`${api}`, (req, res) => {
   res.send(`<h1>RESTful running in root</h1> <p> atomic: <b>${api}/api-docs</b></p>`);
 });
+
 app.get('/atomic', (req, res) => {
   res.send(`<h1>RESTful running in YVAN</h1> <p> eCommerce: <b>${api}/api-docs</b></p>`);
 });
